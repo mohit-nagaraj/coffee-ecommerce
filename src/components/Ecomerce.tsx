cc3c8ac5762f5e21a586d1181f56ae6b748123d1
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-07-07 19:05:12 +0530

    Update Ecomerce.tsx

--- src/components/Ecomerce.tsx
+++ src/components/Ecomerce.tsx
@@ -110,9 +110,7 @@ const ECommerce: React.FC = () => {
         {/* <ChartTwo /> */}
         {/* <ChartThree /> */}
         {/* <MapOne /> */}
-        <div className="col-span-12 xl:col-span-8">
-          {/* <TableOne /> */}
-        </div>
+        <div className="col-span-12 xl:col-span-8">{/* <TableOne /> */}</div>
         {/* <ChatCard /> */}
       </div>
     </>
